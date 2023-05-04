46f95909719f5a25fda5aaa582903430b9fbb278
zhouming <freesky0418@163.com>
2023-05-04 22:41:48 +0800

    使用导航控制器

--- Support/AppDelegate.swift
+++ Support/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow();
-        window?.rootViewController = FCFileListViewController.build();
+        let nav = UINavigationController(rootViewController: FCFileListViewController.build()!)
+        nav.setNavigationBarHidden(true, animated: false)
+        window?.rootViewController = nav
         window?.makeKeyAndVisible();
         return true
     }
